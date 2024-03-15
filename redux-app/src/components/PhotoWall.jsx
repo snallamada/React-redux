945e525b9e45e6f6111bfe7d5fc3aebd47fe2af8
snallamada <shravanreddynallamada@gmail.com>
2024-03-15 17:33:01 +0100

    code added

--- redux-app/src/components/PhotoWall.jsx
+++ redux-app/src/components/PhotoWall.jsx
@@ -1,9 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import './PhotoWall.css'
 
-const PhotoWall = () => {
+
+
+const PhotoWall = ({posts}) => {
+
   return (
-    <div><h1>PhotoWall</h1></div>
+    <><div><h1>PhotoWall</h1></div>
+   
+        {posts.map(post => <div className='photoGrid'>
+        <figure className='figure'> 
+        <img className="photo" src={post.imageLink}/>
+          <center><figcaption><h2>{post.description}</h2></figcaption></center>
+          <div className='button-container'>
+            <button className='button'>Remove</button></div></figure>
+            </div>
+            )}
+            
+        </>
+
+
   )
 }
 
